ca267c42be6a4b80e2a83a529e0f84eca868549f
Benjamin Vetter <benjamin.vetter@wlw.de>
2020-02-18 17:27:02 +0100

    Fix babel

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,15 @@ module.exports = {
   entry: {
     'routing-pattern': path.join(__dirname, 'src/index.js')
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
   // Use globalObject: 'this' to make the library work in browser and node
   // see https://webpack.js.org/configuration/output/#outputlibrary
   output: {
